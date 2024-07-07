9cfae885dac458f98e25efceecdf97dba0a18371
GongDoMin <yhw198@naver.com>
2024-07-08 01:54:18 +0900

    [feat] loginActivity 생성 및 설정

---
+++ app/src/main/java/com/prac/githubrepo/login/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.prac.githubrepo.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.prac.githubrepo.R
+import com.prac.githubrepo.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private val viewModel: LoginViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
