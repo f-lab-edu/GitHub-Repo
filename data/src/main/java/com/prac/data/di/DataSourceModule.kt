c0e7aa905632e1cb99147bd15fcd6aace8f8a04d
GongDoMin <yhw198@naver.com>
2024-07-09 01:49:18 +0900

    [feat] dataSoruce DI 설정

---
+++ data/src/main/java/com/prac/data/di/DataSourceModule.kt
@@ -0,0 +1,22 @@
+package com.prac.data.di
+
+import com.prac.data.source.GetAccessTokenDataSource
+import com.prac.data.source.api.GitHubTokenApi
+import com.prac.data.source.impl.GetAccessTokenDataSourceImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DataSourceModule {
+    @Provides
+    @Singleton
+    fun provideGetAccessTokenDataSource(
+        gitHubTokenApi: GitHubTokenApi
+    ): GetAccessTokenDataSource =
+        GetAccessTokenDataSourceImpl(gitHubTokenApi)
+}
