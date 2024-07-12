3ec00e38625ad4c241fed51053b03498243692fc
GongDoMin <yhw198@naver.com>
2024-07-12 09:31:11 +0900

    [refactor] OkHttpClient 설정 값 추가, conver factory DI, Retrofit 설정 값 변경

--- data/src/main/java/com/prac/data/di/RetrofitModule.kt
+++ data/src/main/java/com/prac/data/di/RetrofitModule.kt
@@ -1,6 +1,8 @@
 package com.prac.data.di
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.prac.data.BuildConfig
+import com.prac.data.source.api.ErrorHandleInterceptor
 import com.prac.data.source.api.GitHubTokenApi
 import dagger.Module
 import dagger.Provides
@@ -9,7 +11,10 @@ import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -17,21 +22,35 @@ import javax.inject.Singleton
 internal object RetrofitModule {
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient =
-        OkHttpClient.Builder()
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(timeout = 5, unit = TimeUnit.SECONDS)
+            .readTimeout(timeout = 5, unit = TimeUnit.SECONDS)
+            .writeTimeout(timeout = 5, unit = TimeUnit.SECONDS)
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BASIC else HttpLoggingInterceptor.Level.NONE
+                }
+            )
+            .addInterceptor(ErrorHandleInterceptor())
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSerializationConverter(): Converter.Factory =
+        Json.asConverterFactory("application/json".toMediaType())
 
     @Provides
     @Singleton
     fun provideGitHubTokenRetrofit(
-        okHttpClient: OkHttpClient
+        okHttpClient: OkHttpClient,
+        converterFactory: Converter.Factory
     ): Retrofit =
         Retrofit.Builder()
-            .baseUrl("https://github.com/")
+            .baseUrl(BuildConfig.GITHUB_URL)
             .client(okHttpClient)
-            .addConverterFactory(
-                Json.asConverterFactory("application/json".toMediaType())
-            )
+            .addConverterFactory(converterFactory)
             .build()
 
     @Provides
