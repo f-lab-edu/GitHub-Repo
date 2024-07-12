c6a409f3edf7d935b484b6338257e834f2331f09
GongDoMin <yhw198@naver.com>
2024-07-12 09:31:37 +0900

    [refactor] AccessTokenModel camel case로 변경

--- data/src/main/java/com/prac/data/source/model/AccessTokenModel.kt
+++ data/src/main/java/com/prac/data/source/model/AccessTokenModel.kt
@@ -1,10 +1,11 @@
 package com.prac.data.source.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 internal data class AccessTokenModel(
-    val access_token: String,
-    val scope: String,
-    val token_type: String
+    @SerialName("access_token") val accessToken: String,
+    @SerialName("scope") val scope: String,
+    @SerialName("token_type") val tokenType: String
 )
