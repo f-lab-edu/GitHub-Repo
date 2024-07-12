020f64796e9c59b0a9badbfff0e688860d854f3e
GongDoMin <yhw198@naver.com>
2024-07-12 09:32:19 +0900

    [refactor] GitHubTokenApi suspend fun으로 변경

--- data/src/main/java/com/prac/data/source/api/GitHubTokenApi.kt
+++ data/src/main/java/com/prac/data/source/api/GitHubTokenApi.kt
@@ -1,18 +1,16 @@
 package com.prac.data.source.api
 
 import com.prac.data.source.model.AccessTokenModel
-import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 internal interface GitHubTokenApi {
     @POST("login/oauth/access_token")
-    fun getAccessToken(
+    suspend fun getAccessToken(
         @Header("Accept") accept: String = "application/json",
         @Query("client_id") clientID: String,
         @Query("client_secret") clientSecret: String,
         @Query("code") code: String,
-    ): Response<AccessTokenModel>
+    ): AccessTokenModel
 }
