d34cd4e310032db0af5bbde6c5ce4ae7e7bfc543
GongDoMin <yhw198@naver.com>
2024-07-12 09:38:59 +0900

    [refactor] access token data source suspend fun 변경 및 flow 제거

--- data/src/main/java/com/prac/data/source/GetAccessTokenDataSource.kt
+++ data/src/main/java/com/prac/data/source/GetAccessTokenDataSource.kt
@@ -1,12 +1,11 @@
 package com.prac.data.source
 
 import com.prac.data.repository.dto.AccessTokenDto
-import kotlinx.coroutines.flow.Flow
 
 internal interface GetAccessTokenDataSource {
-    fun getAccessToken(
+    suspend fun getAccessToken(
         clientID: String,
         clientSecret: String,
         code: String,
-    ) : Flow<AccessTokenDto>
+    ) : AccessTokenDto
 }
--- data/src/main/java/com/prac/data/source/impl/GetAccessTokenDataSourceImpl.kt
+++ data/src/main/java/com/prac/data/source/impl/GetAccessTokenDataSourceImpl.kt
@@ -3,43 +3,25 @@ package com.prac.data.source.impl
 import com.prac.data.repository.dto.AccessTokenDto
 import com.prac.data.source.GetAccessTokenDataSource
 import com.prac.data.source.api.GitHubTokenApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import java.io.IOException
 import javax.inject.Inject
 
 internal class GetAccessTokenDataSourceImpl @Inject constructor(
     private val gitHubTokenApi: GitHubTokenApi
 ) : GetAccessTokenDataSource {
-    override fun getAccessToken(
+    override suspend fun getAccessToken(
         clientID: String,
         clientSecret: String,
         code: String
-    ): Flow<AccessTokenDto> {
-        return try {
-            flow {
-                val response = gitHubTokenApi.getAccessToken(
-                    clientID = clientID,
-                    clientSecret = clientSecret,
-                    code = code
-                )
+    ): AccessTokenDto {
+        val response = gitHubTokenApi.getAccessToken(
+            clientID = clientID,
+            clientSecret = clientSecret,
+            code = code
+        )
 
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        emit(
-                            AccessTokenDto(
-                                accessToken = it.access_token,
-                                tokenType = it.token_type
-                            )
-                        )
-                    } ?: throw IOException()
-                } else {
-                    throw IOException()
-                }
-            }
-        } catch (e: Exception) {
-            throw e
-        }
-
+        return AccessTokenDto(
+            accessToken = response.accessToken,
+            tokenType = response.tokenType
+        )
     }
 }
