a27ca3c308e34f96a1b1c145525feed2d6639224
GongDoMin <yhw198@naver.com>
2024-07-14 22:52:21 +0900

    [refactor] loginUIState 수정

--- app/src/main/java/com/prac/githubrepo/login/LoginUIState.kt
+++ app/src/main/java/com/prac/githubrepo/login/LoginUIState.kt
@@ -1,7 +1,6 @@
 package com.prac.githubrepo.login
 
 data class LoginUIState(
-    val accessToken: String? = null,
     val isLoading: Boolean = false,
-    val loginException: Exception? = null
+    val userMessage: String? = null
 )
