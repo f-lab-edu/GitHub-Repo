a9d6c414ec165b22fed306cdf6c328c49e2f4561
GongDoMin <yhw198@naver.com>
2024-07-15 00:51:52 +0900

    [feat] UIState 변수, accessToken 변수 생성

--- app/src/main/java/com/prac/githubrepo/login/LoginViewModel.kt
+++ app/src/main/java/com/prac/githubrepo/login/LoginViewModel.kt
@@ -3,11 +3,17 @@ package com.prac.githubrepo.login
 import androidx.lifecycle.ViewModel
 import com.prac.data.repository.TokenRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val tokenRepository: TokenRepository
 ): ViewModel() {
+    private val _uiState = MutableStateFlow(LoginUIState())
+    val uiState = _uiState.asStateFlow()
 
+    private val _accessToken = MutableStateFlow<String?>(null)
+    val accessToken = _accessToken.asStateFlow()
 }
