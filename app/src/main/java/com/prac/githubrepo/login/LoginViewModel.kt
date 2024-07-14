e696fe497d6f712257f2311c6d5738c3d8ed6d1d
GongDoMin <yhw198@naver.com>
2024-07-15 00:49:46 +0900

    [feat] tokenRepository DI

--- app/src/main/java/com/prac/githubrepo/login/LoginViewModel.kt
+++ app/src/main/java/com/prac/githubrepo/login/LoginViewModel.kt
@@ -1,7 +1,13 @@
 package com.prac.githubrepo.login
 
 import androidx.lifecycle.ViewModel
+import com.prac.data.repository.TokenRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val tokenRepository: TokenRepository
+): ViewModel() {
 
 }
