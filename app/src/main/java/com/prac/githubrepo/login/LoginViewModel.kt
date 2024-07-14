0086e52ed303930761443033db09b391c3700785
GongDoMin <yhw198@naver.com>
2024-07-15 01:36:29 +0900

    [feat] 엑세스 토큰 가져오는 비즈니스 로직 추가

--- app/src/main/java/com/prac/githubrepo/login/LoginViewModel.kt
+++ app/src/main/java/com/prac/githubrepo/login/LoginViewModel.kt
@@ -1,10 +1,13 @@
 package com.prac.githubrepo.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prac.data.exception.GitHubApiException
 import com.prac.data.repository.TokenRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,4 +19,26 @@ class LoginViewModel @Inject constructor(
 
     private val _accessToken = MutableStateFlow<String?>(null)
     val accessToken = _accessToken.asStateFlow()
+
+    fun fetchAccessToken(clientID: String, clientSecret: String, code: String) {
+        viewModelScope.launch {
+            _uiState.value = LoginUIState(isLoading = true)
+
+            tokenRepository.getAccessTokenApi(clientID = clientID, clientSecret = clientSecret, code = code)
+                .onSuccess {
+                    _accessToken.value = it.accessToken
+                    _uiState.value = LoginUIState(isLoading = false)
+                }.onFailure {
+                    when (it) {
+                        is GitHubApiException.NetworkException, is GitHubApiException.UnAuthorizedException -> {
+                            _uiState.value = LoginUIState(isLoading = false, userMessage = it.message)
+                        }
+                        else -> {
+                            _uiState.value = LoginUIState(isLoading = false, userMessage = "알 수 없는 오류가 발생했습니다.")
+                        }
+                    }
+                }
+        }
+    }
+
 }
