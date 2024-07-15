95316b9f38725f8d4e24e088e4a015ad98a222b6
GongDoMin <yhw198@naver.com>
2024-07-15 14:18:07 +0900

    [feat] sharedPreferences DI 생성

---
+++ data/src/main/java/com/prac/data/di/SharedPreferencesModule.kt
@@ -0,0 +1,19 @@
+package com.prac.data.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SharedPreferencesModule {
+    @Provides
+    @Singleton
+    fun provideTokenSharedPreferences(@ApplicationContext context: Context) : SharedPreferences =
+        context.getSharedPreferences("token", Context.MODE_PRIVATE)
+}
