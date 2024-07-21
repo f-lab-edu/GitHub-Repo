d479631b1c4b5ca7d83d56dbbdb8cb99557f7009
GongDoMin <yhw198@naver.com>
2024-07-21 10:43:10 +0900

    [feat] githubOAuth 연동

--- app/src/main/java/com/prac/githubrepo/login/LoginActivity.kt
+++ app/src/main/java/com/prac/githubrepo/login/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.prac.githubrepo.login
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.prac.githubrepo.BuildConfig
 import com.prac.githubrepo.R
 import com.prac.githubrepo.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,5 +25,10 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    private fun startGitHubOAuth() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(BuildConfig.GITHUB_OAUTH_URI))
+        startActivity(intent)
     }
 }
