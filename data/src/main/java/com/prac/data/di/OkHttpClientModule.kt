2911fc1995b4501bcb9d0bdefa262c5fc8a7bdc9
GongDoMin <yhw198@naver.com>
2024-07-23 01:57:17 +0900

    [feat] BasicOkHttpClient DI 추가

--- data/src/main/java/com/prac/data/di/OkHttpClientModule.kt
+++ data/src/main/java/com/prac/data/di/OkHttpClientModule.kt
@@ -1,5 +1,7 @@
 package com.prac.data.di
 
+import com.prac.data.BuildConfig
+import com.prac.data.di.annotation.BasicOkHttpClient
 import com.prac.data.di.binds.TokenSharedPreferences
 import com.prac.data.source.api.AuthorizationInterceptor
 import dagger.Module
@@ -7,6 +9,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -15,5 +21,18 @@ object OkHttpClientModule {
     fun provideAuthorizationInterceptor(tokenSharedPreferences: TokenSharedPreferences) : Interceptor =
         AuthorizationInterceptor(tokenSharedPreferences)
 
-
+    @Provides
+    @Singleton
+    @BasicOkHttpClient
+    fun provideBasicOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(timeout = 5, unit = TimeUnit.SECONDS)
+            .readTimeout(timeout = 5, unit = TimeUnit.SECONDS)
+            .writeTimeout(timeout = 5, unit = TimeUnit.SECONDS)
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BASIC else HttpLoggingInterceptor.Level.NONE
+                }
+            )
+            .build()
 }
