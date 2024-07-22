fe231fe8767a4677de7b2e65bef692f7db6e8346
GongDoMin <yhw198@naver.com>
2024-07-23 01:50:47 +0900

    [feat] AuthorizationInterceptor 추가

---
+++ data/src/main/java/com/prac/data/source/api/AuthorizationInterceptor.kt
@@ -0,0 +1,28 @@
+package com.prac.data.source.api
+
+import com.prac.data.di.binds.TokenSharedPreferences
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthorizationInterceptor @Inject constructor(
+    private val tokenSharedPreferences: TokenSharedPreferences
+) : Interceptor {
+    companion object {
+        private const val AUTHORIZATION = "Authorization"
+        private const val AUTHORIZATION_TYPE = "Bearer"
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken = tokenSharedPreferences.getToken(TokenSharedPreferences.KEY.ACCESS_TOKEN)
+        val request = chain.request().putTokenHeader(accessToken)
+        return chain.proceed(request)
+    }
+
+    private fun Request.putTokenHeader(accessToken: String) : Request {
+        return this.newBuilder()
+            .addHeader(AUTHORIZATION, "$AUTHORIZATION_TYPE $accessToken")
+            .build()
+    }
+}
