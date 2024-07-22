2c5ecfcf300dd09465066fc0e74ea988e09fca32
GongDoMin <yhw198@naver.com>
2024-07-23 01:53:39 +0900

    [feat] OkHttpClientAnnotation 추가

---
+++ data/src/main/java/com/prac/data/di/annotation/OkHttpClientAnnotation.kt
@@ -0,0 +1,11 @@
+package com.prac.data.di.annotation
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BasicOkHttpClient
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AuthorizationInterceptorOkHttpClient
