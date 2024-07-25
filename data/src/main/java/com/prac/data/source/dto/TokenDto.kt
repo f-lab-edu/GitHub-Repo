cf0dfb6aac457ac216abcb8bac1a65e8a09632fb
GongDoMin <yhw198@naver.com>
2024-07-25 19:53:24 +0900

    [refactor] tokenDto default value 추가

--- data/src/main/java/com/prac/data/source/dto/TokenDto.kt
+++ data/src/main/java/com/prac/data/source/dto/TokenDto.kt
@@ -5,10 +5,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 internal data class TokenDto(
-    @SerialName("access_token") val accessToken: String,
-    @SerialName("expires_in") val expiresIn: Int,
-    @SerialName("refresh_token") val refreshToken: String,
-    @SerialName("refresh_token_expires_in") val refreshTokenExpiresIn: Int,
-    @SerialName("scope") val scope: String,
-    @SerialName("token_type") val tokenType: String
+    @SerialName("access_token") val accessToken: String = "",
+    @SerialName("expires_in") val expiresIn: Int = 0,
+    @SerialName("refresh_token") val refreshToken: String = "",
+    @SerialName("refresh_token_expires_in") val refreshTokenExpiresIn: Int = 0,
+    @SerialName("scope") val scope: String = "",
+    @SerialName("token_type") val tokenType: String = ""
 )
