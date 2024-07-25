629efa145ab2dce8858ad595b20cc66561a6d49c
GongDoMin <yhw198@naver.com>
2024-07-25 19:53:53 +0900

    [refactor] RepoDto default value 추가

--- data/src/main/java/com/prac/data/source/dto/RepoDto.kt
+++ data/src/main/java/com/prac/data/source/dto/RepoDto.kt
@@ -6,11 +6,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 internal data class RepoDto(
-    @SerialName("id") val id: Int,
-    @SerialName("name") val name: String,
-    @SerialName("owner") val owner: OwnerDto,
-    @SerialName("stargazers_count") val stargazersCount: Int,
-    @SerialName("updated_at") val updatedAt: String
+    @SerialName("id") val id: Int = 0,
+    @SerialName("name") val name: String = "",
+    @SerialName("owner") val owner: OwnerDto = OwnerDto(),
+    @SerialName("stargazers_count") val stargazersCount: Int = 0,
+    @SerialName("updated_at") val updatedAt: String = ""
 ) {
     fun toModel() =
         RepoModel(id, name, owner.toModel(), stargazersCount, updatedAt)
