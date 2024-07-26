461e24100800219c5d6e5be0d81247f9f9805060
GongDoMin <yhw198@naver.com>
2024-07-26 13:57:46 +0900

    [feat] viewModel 설정

--- app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
+++ app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.prac.githubrepo.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.prac.githubrepo.R
 import com.prac.githubrepo.databinding.ActivityMainBinding
@@ -9,6 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
