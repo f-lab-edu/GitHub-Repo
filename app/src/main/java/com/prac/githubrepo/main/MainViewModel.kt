505b76d61af3da9ef24355a9366de170eaff8ce1
GongDoMin <yhw198@naver.com>
2024-07-26 11:50:54 +0900

    [feat] getRepositories 추가

--- app/src/main/java/com/prac/githubrepo/main/MainViewModel.kt
+++ app/src/main/java/com/prac/githubrepo/main/MainViewModel.kt
@@ -1,12 +1,15 @@
 package com.prac.githubrepo.main
 
-import android.provider.Contacts.Intents.UI
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.prac.data.entity.RepoEntity
+import com.prac.data.exception.GitHubApiException
 import com.prac.data.repository.RepoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,4 +32,28 @@ class MainViewModel @Inject constructor(
 
     private val _uiState = MutableStateFlow<UiState>(UiState.Idle)
     val uiState = _uiState.asStateFlow()
+
+    fun getRepositories() {
+        viewModelScope.launch {
+            if (_uiState.value != UiState.Idle) return@launch
+
+            _uiState.update { UiState.Loading }
+
+            // AccessToken 으로 유저 정보 가져오는 API 추가할 예정
+            repoRepository.getRepositories("GongDoMin")
+                .onSuccess { repoRepositories ->
+                    _uiState.update { UiState.Success(repoRepositories) }
+                }
+                .onFailure { throwable ->
+                    when (throwable) {
+                        is GitHubApiException.NetworkException, is GitHubApiException.UnAuthorizedException -> {
+                            _uiState.update { UiState.Error(throwable.message ?: "레파지토리를 불러오는데 실패했습니다.") }
+                        }
+                        else -> {
+                            _uiState.update { UiState.Error("알 수 없는 에러가 발생했습니다.") }
+                        }
+                    }
+                }
+        }
+    }
 }
