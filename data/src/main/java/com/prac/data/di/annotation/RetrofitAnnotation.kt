9784cdfcfde6fb894258528ea6764e8c5b5d8042
GongDoMin <yhw198@naver.com>
2024-07-26 10:26:38 +0900

    [feat] RetrofitAnnotation 추가

---
+++ data/src/main/java/com/prac/data/di/annotation/RetrofitAnnotation.kt
@@ -0,0 +1,11 @@
+package com.prac.data.di.annotation
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class GitHubTokenRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class GitHubRetrofit
