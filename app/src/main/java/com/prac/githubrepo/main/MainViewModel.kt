56a39dd296fbbe1bda0cb0ac77140eefaa2c4c50
GongDoMin <yhw198@naver.com>
2024-07-26 11:09:00 +0900

    [feat] UiState sealed class 생성

--- app/src/main/java/com/prac/githubrepo/main/MainViewModel.kt
+++ app/src/main/java/com/prac/githubrepo/main/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.prac.githubrepo.main
 
+import android.provider.Contacts.Intents.UI
 import androidx.lifecycle.ViewModel
+import com.prac.data.entity.RepoEntity
 import com.prac.data.repository.RepoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,4 +11,17 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val repoRepository: RepoRepository
 ): ViewModel() {
+    sealed class UiState {
+        data object Idle : UiState()
+
+        data object Loading : UiState()
+
+        data class Success(
+            val repositories : List<RepoEntity>
+        ) : UiState()
+
+        data class Error(
+            val errorMessage: String
+        ) : UiState()
+    }
 }
