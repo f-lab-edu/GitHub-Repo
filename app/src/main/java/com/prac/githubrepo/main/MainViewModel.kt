f99f5640076f2583d38a2da732f83b21ad62c779
GongDoMin <yhw198@naver.com>
2024-07-26 10:43:18 +0900

    [feat] repoRepository DI

--- app/src/main/java/com/prac/githubrepo/main/MainViewModel.kt
+++ app/src/main/java/com/prac/githubrepo/main/MainViewModel.kt
@@ -1,6 +1,12 @@
 package com.prac.githubrepo.main
 
 import androidx.lifecycle.ViewModel
+import com.prac.data.repository.RepoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repoRepository: RepoRepository
+): ViewModel() {
 }
