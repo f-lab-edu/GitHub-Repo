7114f7f40f670e121a7277798a4f42425d3213b7
GongDoMin <yhw198@naver.com>
2024-07-27 21:33:17 +0900

    [refactor] accessToken runBlocking 추가

--- data/src/main/java/com/prac/data/source/api/AuthorizationInterceptor.kt
+++ data/src/main/java/com/prac/data/source/api/AuthorizationInterceptor.kt
@@ -1,20 +1,23 @@
 package com.prac.data.source.api
 
-import com.prac.data.di.binds.TokenSharedPreferences
+import com.prac.data.di.datastore.TokenDataStoreManager
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 import javax.inject.Inject
 
 class AuthorizationInterceptor @Inject constructor(
-    private val tokenSharedPreferences: TokenSharedPreferences
+    private val tokenDataStoreManager: TokenDataStoreManager
 ) : Interceptor {
     companion object {
         private const val AUTHORIZATION = "Authorization"
         private const val AUTHORIZATION_TYPE = "Bearer"
     }
 
-    private val accessToken = tokenSharedPreferences.getToken(TokenSharedPreferences.KEY.ACCESS_TOKEN)
+    private val accessToken = runBlocking {
+        tokenDataStoreManager.getToken(TokenDataStoreManager.KEY.ACCESS_TOKEN)
+    }
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().putTokenHeader(accessToken)
