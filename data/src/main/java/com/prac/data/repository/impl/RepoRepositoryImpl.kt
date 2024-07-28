a8d0b87033d2ab8edd30ed13545a9f6b02167b72
GongDoMin <yhw198@naver.com>
2024-07-28 19:10:53 +0900

    [refactor] RepoRepositoryImpl getRepositories 내부 구현 코드 변경

--- data/src/main/java/com/prac/data/repository/impl/RepoRepositoryImpl.kt
+++ data/src/main/java/com/prac/data/repository/impl/RepoRepositoryImpl.kt
@@ -1,20 +1,33 @@
 package com.prac.data.repository.impl
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
 import com.prac.data.entity.RepoEntity
 import com.prac.data.repository.RepoRepository
 import com.prac.data.source.RepoApiDataSource
+import com.prac.data.source.impl.RepoApiDataSourceImpl.Companion.PAGE_SIZE
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 internal class RepoRepositoryImpl @Inject constructor(
     private val repoApiDataSource: RepoApiDataSource
 ) : RepoRepository {
-    override suspend fun getRepositories(userName: String): Result<List<RepoEntity>> {
-        try {
-            val dto = repoApiDataSource.getRepos(userName)
-
-            return Result.success(dto.map { it.toEntity() })
+    override suspend fun getRepositories(): Result<PagingData<RepoEntity>> {
+        return try {
+            Result.success(
+                Pager(
+                    config = PagingConfig(
+                        pageSize = PAGE_SIZE,
+                        enablePlaceholders = false
+                    ),
+                    pagingSourceFactory = { repoApiDataSource }
+                ).flow.first().map { it.toEntity() }
+            )
         } catch (e: Exception) {
-            return Result.failure(e)
+            Result.failure(e)
         }
     }
 }
+
