f272bb6f468d4d37e4e4a433f68cf007abed14dd
GongDoMin <yhw198@naver.com>
2024-08-04 01:11:37 +0900

    [feat] add AndroidEntryPoint annotation

--- app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
+++ app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
@@ -3,7 +3,9 @@ package com.prac.githubrepo.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.prac.githubrepo.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
