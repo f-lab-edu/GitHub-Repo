6aa9c5d8fdfb92cdedb9f8979a9b8f9cf3b0a5c3
GongDoMin <yhw198@naver.com>
2024-08-03 10:21:48 +0900

    [refactor] update getRepositories logic and return type

--- data/src/main/java/com/prac/data/repository/impl/RepoRepositoryImpl.kt
+++ data/src/main/java/com/prac/data/repository/impl/RepoRepositoryImpl.kt
@@ -8,26 +8,25 @@ import com.prac.data.entity.RepoEntity
 import com.prac.data.repository.RepoRepository
 import com.prac.data.source.RepoApiDataSource
 import com.prac.data.source.impl.RepoApiDataSourceImpl.Companion.PAGE_SIZE
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 internal class RepoRepositoryImpl @Inject constructor(
     private val repoApiDataSource: RepoApiDataSource
 ) : RepoRepository {
-    override suspend fun getRepositories(): Result<PagingData<RepoEntity>> {
-        return try {
-            Result.success(
-                Pager(
-                    config = PagingConfig(
-                        pageSize = PAGE_SIZE,
-                        enablePlaceholders = false
-                    ),
-                    pagingSourceFactory = { repoApiDataSource }
-                ).flow.first().map { it.toEntity() }
-            )
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
+    override suspend fun getRepositories(): Flow<PagingData<RepoEntity>> =
+        Pager(
+            config = PagingConfig(
+                pageSize = PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { repoApiDataSource }
+        ).flow
+            .map { pagingData ->
+                pagingData.map { repoModel ->
+                    repoModel.toEntity()
+                }
+            }
 }
 
