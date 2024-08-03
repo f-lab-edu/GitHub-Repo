daf572ba8c1c9b886726eed8a786fa7679edba40
GongDoMin <yhw198@naver.com>
2024-08-04 01:13:19 +0900

    [feat] initialize ActivityMainBinding and set contentView

--- app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
+++ app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
@@ -3,12 +3,18 @@ package com.prac.githubrepo.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.prac.githubrepo.R
+import com.prac.githubrepo.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
