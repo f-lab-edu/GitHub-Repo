fc4ceb3a140ad5532dd5e9fde5359730fbd9004b
GongDoMin <yhw198@naver.com>
2024-08-04 01:23:37 +0900

    [feat] initialize adapters and bind concatAdapter

--- app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
+++ app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.prac.githubrepo.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ConcatAdapter
 import com.prac.githubrepo.R
 import com.prac.githubrepo.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,11 +13,16 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
+    private val mainAdapter: MainAdapter by lazy { MainAdapter() }
+    private val retryFooterAdapter: RetryFooterAdapter by lazy { RetryFooterAdapter { mainAdapter.retry() } }
+    private val conCatAdapter: ConcatAdapter by lazy { ConcatAdapter(mainAdapter, retryFooterAdapter) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rvMain.adapter = conCatAdapter
     }
 }
