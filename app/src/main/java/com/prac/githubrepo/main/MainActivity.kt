cc70145d5870ce0a78d2d56821cb22b64217ce83
GongDoMin <yhw198@naver.com>
2024-08-05 13:44:12 +0900

    [refactor] change load state assignment to use viewModel.setLoadState

--- app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
+++ app/src/main/java/com/prac/githubrepo/main/MainActivity.kt
@@ -51,11 +51,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun CombinedLoadStates.handleLoadStates() {
         if (source.refresh is LoadState.Error || source.refresh is LoadState.Loading) {
-            retryFooterAdapter.loadState = source.refresh
+            viewModel.setLoadState(source.refresh)
             return
         }
 
-        retryFooterAdapter.loadState = source.append
+        viewModel.setLoadState(source.append)
     }
 
     private suspend fun UiState.handleUiState() {
