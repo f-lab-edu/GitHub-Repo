f2bcfda73563427e2105408218b97d29a5de00d3
victhor <victhor@daangn.com>
2024-08-06 04:16:31 +0900

    feat: AuthorizationInterceptor 에서 expired 상태라면 401 을 반환하도록 변경

--- data/src/main/java/com/prac/data/source/api/AuthorizationInterceptor.kt
+++ data/src/main/java/com/prac/data/source/api/AuthorizationInterceptor.kt
@@ -12,10 +12,20 @@ internal class AuthorizationInterceptor @Inject constructor(
     companion object {
         private const val AUTHORIZATION = "Authorization"
         private const val AUTHORIZATION_TYPE = "Bearer"
+
+        private val UNAUTHORIZED = Response.Builder()
+            .code(401)
+            .message("Unauthorized")
+            .build()
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val accessToken = runBlocking { tokenDataStoreManager.getToken() }
+        if (accessToken.isExpired) {
+            // TODO refresh access token by refresh token
+            // 하지만 refresh 로직은 따로 구현하는 것으로 하고 여기서는 401을 리턴한다.
+            return UNAUTHORIZED
+        }
         val request = chain.request().newBuilder()
             .addHeader(AUTHORIZATION, "$AUTHORIZATION_TYPE $accessToken")
             .build()
