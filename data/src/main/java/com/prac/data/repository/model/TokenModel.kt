eba48f2832a787e8f72237f5baec5bac3976be2e
victhor <victhor@daangn.com>
2024-08-06 03:34:44 +0900

    feat: token model 내부에 expired, refresh token expired 로직 추가

--- data/src/main/java/com/prac/data/repository/model/TokenModel.kt
+++ data/src/main/java/com/prac/data/repository/model/TokenModel.kt
@@ -8,4 +8,10 @@ internal data class TokenModel(
     val expiresIn: Int,
     val refreshTokenExpiresIn: Int,
     val updatedAt: ZonedDateTime
-)
+) {
+    val isExpired: Boolean
+        get() = updatedAt.plusSeconds(expiresIn.toLong()).isBefore(ZonedDateTime.now())
+
+    val isRefreshTokenExpired: Boolean
+        get() = updatedAt.plusSeconds(refreshTokenExpiresIn.toLong()).isBefore(ZonedDateTime.now())
+}
