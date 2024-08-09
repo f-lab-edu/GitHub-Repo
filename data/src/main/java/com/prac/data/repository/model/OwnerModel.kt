250291a36e60af5e871b2f81623eb88e20ebccac
GongDoMin <yhw198@naver.com>
2024-08-09 16:25:50 +0900

    refact: OwnerModel toEntity 함수 제거

--- data/src/main/java/com/prac/data/repository/model/OwnerModel.kt
+++ data/src/main/java/com/prac/data/repository/model/OwnerModel.kt
@@ -1,11 +1,6 @@
 package com.prac.data.repository.model
 
-import com.prac.data.entity.OwnerEntity
-
 internal data class OwnerModel(
     val login: String,
     val avatarUrl: String
-) {
-    fun toEntity() =
-        OwnerEntity(login, avatarUrl)
-}
+)
