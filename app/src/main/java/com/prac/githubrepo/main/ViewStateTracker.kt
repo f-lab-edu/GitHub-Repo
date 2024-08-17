0090ac9b1c0067f29eaba029b62744db18c89113
GongDoMin <yhw198@naver.com>
2024-08-18 02:15:22 +0900

    feat: jobID, scope, isAttachedStateListenerAdded, attachedStateListenr 변수 생성

--- app/src/main/java/com/prac/githubrepo/main/ViewStateTracker.kt
+++ app/src/main/java/com/prac/githubrepo/main/ViewStateTracker.kt
@@ -3,10 +3,56 @@ package com.prac.githubrepo.main
 import android.view.View
 import com.prac.data.entity.RepoEntity
 import com.prac.data.repository.RepoRepository
+import com.prac.githubrepo.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class ViewStateTracker private constructor(
     private val repoRepository: RepoRepository,
     private val view: View,
     private var repoEntity: RepoEntity,
     private val uiStateUpdater: UiStateUpdater
-)
+) {
+    companion object {
+        private val jobID = R.string.jobID
+    }
+
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private var isAttachedStateListenerAdded = false
+    private val attachedStateListener = object : View.OnAttachStateChangeListener {
+        override fun onViewAttachedToWindow(view: View) {
+            view.setCheckIsStarredJob()
+        }
+
+        override fun onViewDetachedFromWindow(view: View) {
+
+        }
+    }
+
+    private fun View.getJob() : Job? {
+        val tag = getTag(jobID)
+
+        if (tag is Job) {
+            return tag
+        }
+
+        return null
+    }
+
+    private fun View.setCheckIsStarredJob() {
+        setTag(
+            jobID,
+            scope.launch {
+                repoRepository.isStarred(repoEntity.name)
+                    .onSuccess {
+                        uiStateUpdater.updateIsStarred(repoEntity.id, it)
+                    }.onFailure {
+                        uiStateUpdater.updateIsStarred(repoEntity.id, false)
+                    }
+            }
+        )
+    }
+}
