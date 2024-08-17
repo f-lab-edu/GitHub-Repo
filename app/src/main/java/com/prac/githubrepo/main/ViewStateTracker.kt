180f2c3226ba36f320353a39d3bb6b0171ea4bb9
GongDoMin <yhw198@naver.com>
2024-08-18 02:15:22 +0900

    feat: jobID, scope, isAttachedStateListenerAdded, attachedStateListenr 변수 생성

--- app/src/main/java/com/prac/githubrepo/main/ViewStateTracker.kt
+++ app/src/main/java/com/prac/githubrepo/main/ViewStateTracker.kt
@@ -3,10 +3,29 @@ package com.prac.githubrepo.main
 import android.view.View
 import com.prac.data.entity.RepoEntity
 import com.prac.data.repository.RepoRepository
+import com.prac.githubrepo.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 class ViewStateTracker private constructor(
     private val repoRepository: RepoRepository,
     private val view: View,
     private var repoEntity: RepoEntity,
     private val uiStateUpdater: UiStateUpdater
-)
+) {
+    companion object {
+        private val jobID = R.string.jobID
+    }
+
+    private val scope = CoroutineScope(Dispatchers.IO)
+    private var isAttachedStateListenerAdded = false
+    private val attachedStateListener = object : View.OnAttachStateChangeListener {
+        override fun onViewAttachedToWindow(view: View) {
+
+        }
+
+        override fun onViewDetachedFromWindow(view: View) {
+
+        }
+    }
+}
