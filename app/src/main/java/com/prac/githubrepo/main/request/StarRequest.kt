09bc93d4470d1a6fc4cea09f1d2ee6fa2bf8cf15
GongDoMin <yhw198@naver.com>
2024-08-28 23:51:41 +0900

    refact: StarRequest 생성자에 StarStateUpdater 추가

--- app/src/main/java/com/prac/githubrepo/main/request/StarRequest.kt
+++ app/src/main/java/com/prac/githubrepo/main/request/StarRequest.kt
@@ -2,6 +2,7 @@ package com.prac.githubrepo.main.request
 
 import com.prac.data.entity.RepoEntity
 import com.prac.data.repository.RepoRepository
+import com.prac.githubrepo.main.StarStateUpdater
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -9,6 +10,7 @@ import kotlinx.coroutines.launch
 
 class StarRequest internal constructor(
     private val repoRepository: RepoRepository,
+    private val starStateUpdater: StarStateUpdater,
     private val repoEntity: RepoEntity,
     private val scope: CoroutineScope,
 ) : Request {
