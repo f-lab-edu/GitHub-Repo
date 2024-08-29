e6b6b00841b3001ce2f8404ca44465d1a3694423
GongDoMin <yhw198@naver.com>
2024-08-29 11:05:18 +0900

    feat: Service 에서 star repository api 메서드 추가

--- data/src/main/java/com/prac/data/source/api/GitHubApi.kt
+++ data/src/main/java/com/prac/data/source/api/GitHubApi.kt
@@ -3,6 +3,7 @@ package com.prac.data.source.api
 import com.prac.data.source.dto.RepoDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -17,6 +18,12 @@ internal interface GitHubApi {
 
     @GET("user/starred/{userName}/{repoName}")
     suspend fun checkRepositoryIsStarred(
+        @Path("userName") userName: String,
+        @Path("repoName") repoName: String
+    )
+
+    @PUT("user/starred/{userName}/{repoName}")
+    suspend fun starRepository(
         @Path("userName") userName: String,
         @Path("repoName") repoName: String
     )
