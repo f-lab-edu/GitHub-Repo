93e532f900d1f6fe501392211bf1a1ec05783ce6
GongDoMin <yhw198@naver.com>
2024-08-31 23:49:12 +0900

    feat: StarStateUpdater Hilt 를 통해 생성

---
+++ app/src/main/java/com/prac/githubrepo/main/di/StarModule.kt
@@ -0,0 +1,32 @@
+package com.prac.githubrepo.main.di
+
+import com.prac.data.entity.RepoEntity
+import com.prac.data.repository.RepoRepository
+import com.prac.githubrepo.main.StarStateMediator
+import com.prac.githubrepo.main.star.StarStateUpdater
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+class StarModule {
+    @Provides
+    fun provideStarStateFetcher(
+        repoRepository: RepoRepository,
+        starStateMediator: StarStateMediator
+    ) : StarStateUpdater {
+        return object : StarStateUpdater {
+            override suspend fun updateStarState(repoEntity: RepoEntity) {
+                repoRepository.isStarred(repoEntity.name)
+                    .onSuccess {
+                        starStateMediator.addStarState(repoEntity.id, it)
+                    }.onFailure {
+                        starStateMediator.addStarState(repoEntity.id, false)
+                    }
+            }
+
+        }
+    }
+}
