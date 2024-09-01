8c239c249b33a9bd7c7af9e373837e3cb758c0f7
GongDoMin <yhw198@naver.com>
2024-09-01 15:21:30 +0900

    feat: StarState 데이터 클래스 추가

--- app/src/main/java/com/prac/githubrepo/main/StarStateMediator.kt
+++ app/src/main/java/com/prac/githubrepo/main/StarStateMediator.kt
@@ -6,4 +6,10 @@ interface StarStateMediator {
     val starStates: StateFlow<List<Pair<Int, Boolean>>>
 
     fun addStarState(id: Int, isStarred: Boolean)
+
+    data class StarState(
+        val id: Int,
+        val isStarred: Boolean,
+        val stargazersCount: Int
+    )
 }
