bdf09afb3a17856025d2a1701d720e8e60de27ef
GongDoMin <yhw198@naver.com>
2024-09-07 00:39:06 +0900

    feat: repository 상세 정보를 가져오는 getRepository 메서드 구현

--- app/src/main/java/com/prac/githubrepo/main/detail/DetailViewModel.kt
+++ app/src/main/java/com/prac/githubrepo/main/detail/DetailViewModel.kt
@@ -1,11 +1,15 @@
 package com.prac.githubrepo.main.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.prac.data.entity.RepoDetailEntity
 import com.prac.data.repository.RepoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -28,4 +32,29 @@ class DetailViewModel @Inject constructor(
 
     private val _uiState = MutableStateFlow<UiState>(UiState.Idle)
     val uiState = _uiState.asStateFlow()
+
+    fun getRepository(userName: String?, repoName: String?) {
+        if (_uiState.value != UiState.Idle) return
+
+        _uiState.update { UiState.Loading }
+
+        if (userName == null || repoName == null) {
+            _uiState.update { UiState.Error("잘못된 접근입니다.") }
+            return
+        }
+
+        viewModelScope.launch(Dispatchers.IO) {
+            repoRepository.getRepository(userName, repoName)
+                .onSuccess { repoDetailEntity ->
+                    _uiState.update {
+                        UiState.ShowRepository(repoDetailEntity)
+                    }
+                }
+                .onFailure { throwable ->
+                    _uiState.update {
+                        UiState.Error(throwable.message.toString())
+                    }
+                }
+        }
+    }
 }
