5328501247a4005e6536d82dc16cf4f61616a3f4
GongDoMin <yhw198@naver.com>
2024-09-06 19:15:56 +0900

    feat: RepoDetail dto, model 그리고 entity data class 생성

---
+++ data/src/main/java/com/prac/data/source/dto/RepoDetailDto.kt
@@ -0,0 +1,13 @@
+package com.prac.data.source.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class RepoDetailDto(
+    @SerialName("id") val id: Int = 0,
+    @SerialName("name") val name: String = "",
+    @SerialName("owner") val owner: OwnerDto = OwnerDto(),
+    @SerialName("stargazers_count") val stargazersCount: Int = 0,
+    @SerialName("forks_count") val forksCount: Int = 0
+)
