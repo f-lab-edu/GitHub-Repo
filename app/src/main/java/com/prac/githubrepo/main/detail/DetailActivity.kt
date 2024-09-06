2821bc65d71bd038aac65c8b69c15a3237f11035
GongDoMin <yhw198@naver.com>
2024-09-06 16:17:40 +0900

    feat: DetailActivity AndroidEntryPoint 어노테이션 추가 및 viewModel 생성

--- app/src/main/java/com/prac/githubrepo/main/detail/DetailActivity.kt
+++ app/src/main/java/com/prac/githubrepo/main/detail/DetailActivity.kt
@@ -2,11 +2,16 @@ package com.prac.githubrepo.main.detail
 
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.prac.githubrepo.databinding.ActivityDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityDetailBinding
+    private val viewModel: DetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
