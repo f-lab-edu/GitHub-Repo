b069428804139b24de70b7dbdf6823a22acc73e4
GongDoMin <yhw198@naver.com>
2024-09-06 23:22:42 +0900

    feat: DetailActivity 클래스 생성

---
+++ app/src/main/java/com/prac/githubrepo/main/detail/DetailActivity.kt
@@ -0,0 +1,10 @@
+package com.prac.githubrepo.main.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
