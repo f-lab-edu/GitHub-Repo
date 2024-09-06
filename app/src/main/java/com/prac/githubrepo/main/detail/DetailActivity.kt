1bb484a5f655c705095d5584dcb2081509594c46
GongDoMin <yhw198@naver.com>
2024-09-07 00:47:43 +0900

    feat: UiState.handleUiState 메서드 추가 및 uiState collect 추가

--- app/src/main/java/com/prac/githubrepo/main/detail/DetailActivity.kt
+++ app/src/main/java/com/prac/githubrepo/main/detail/DetailActivity.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
+import com.prac.data.entity.RepoDetailEntity
+import com.prac.githubrepo.R
 import com.prac.githubrepo.databinding.ActivityDetailBinding
 import com.prac.githubrepo.main.MainActivity.Companion.REPO_NAME
 import com.prac.githubrepo.main.MainActivity.Companion.USER_NAME
+import com.prac.githubrepo.main.detail.DetailViewModel.UiState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
@@ -27,6 +36,14 @@ class DetailActivity : AppCompatActivity() {
             intent.getStringExtra(USER_NAME),
             intent.getStringExtra(REPO_NAME)
         )
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    it.handleUiState()
+                }
+            }
+        }
     }
 
     private fun handleOnBackPressed() {
@@ -35,5 +52,37 @@ class DetailActivity : AppCompatActivity() {
                 finish()
             }
         })
+    }
+
+    private fun UiState.handleUiState() {
+        when (this) {
+            is UiState.Idle -> {}
+            is UiState.Loading -> {
+                binding.includeProgressBar.root.isVisible = true
+            }
+            is UiState.ShowRepository -> {
+                binding.includeProgressBar.root.isVisible = false
+
+                bindRepositoryDetail(this.repository)
+            }
+            is UiState.Error -> {
+                binding.includeProgressBar.root.isVisible = false
+
+                // TODO UIState update to Error
+            }
+        }
+    }
+
+    private fun bindRepositoryDetail(repoDetailEntity: RepoDetailEntity) {
+        Glide.with(binding.root)
+            .load(repoDetailEntity.owner.avatarUrl)
+            .error(R.drawable.img_glide_error)
+            .placeholder(R.drawable.img_glide_profile)
+            .into(binding.ivProfile)
+
+        binding.tvName.text = repoDetailEntity.owner.login
+        binding.tvTitle.text = repoDetailEntity.name
+        binding.tvStarCount.text = getString(R.string.star_count, repoDetailEntity.stargazersCount)
+        binding.tvForkCount.text = getString(R.string.fork_count, repoDetailEntity.forksCount)
     }
 }
