69ceef99253bcf7dbcad6ecc58030901c814d623
GongDoMin <yhw198@naver.com>
2024-09-07 17:41:47 +0900

    feat: StarWorker class 생성

--- app/src/main/java/com/prac/githubrepo/main/work/StarWorkManager.kt
+++ app/src/main/java/com/prac/githubrepo/main/work/StarWorkManager.kt
@@ -1,7 +1,13 @@
 package com.prac.githubrepo.main.work
 
+import android.content.Context
+import androidx.hilt.work.HiltWorker
 import androidx.work.Constraints
+import androidx.work.CoroutineWorker
 import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,4 +16,14 @@ class StarWorkManager @Inject constructor(
     private val workManager: WorkManager,
     private val constraints: Constraints
 ) {
+    @HiltWorker
+    class StarWorker @AssistedInject constructor(
+        @Assisted private val context: Context,
+        @Assisted private val params: WorkerParameters,
+    ) : CoroutineWorker(context, params) {
+        override suspend fun doWork(): Result {
+            // TODO doWork
+            return Result.success()
+        }
+    }
 }
