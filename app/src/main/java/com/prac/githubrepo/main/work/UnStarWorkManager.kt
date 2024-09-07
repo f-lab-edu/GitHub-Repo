81c0cef542c27778c8a812565338695c97b18800
GongDoMin <yhw198@naver.com>
2024-09-07 18:08:52 +0900

    feat: WorkManager Wrapping class UnStarWorkManager 생성

---
+++ app/src/main/java/com/prac/githubrepo/main/work/UnStarWorkManager.kt
@@ -0,0 +1,13 @@
+package com.prac.githubrepo.main.work
+
+import androidx.work.Constraints
+import androidx.work.WorkManager
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UnStarWorkManager @Inject constructor(
+    private val workManager: WorkManager,
+    private val constraints: Constraints
+) {
+}
