9ba2432f021a2cb48cc2278ded529f5e21a54ddf
GongDoMin <yhw198@naver.com>
2024-09-07 17:36:23 +0900

    feat: hilt 를 통해 WorkManager 의존성 주입

---
+++ app/src/main/java/com/prac/githubrepo/main/di/WorkModule.kt
@@ -0,0 +1,20 @@
+package com.prac.githubrepo.main.di
+
+import android.content.Context
+import androidx.work.WorkManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class WorkModule {
+    @Provides
+    @Singleton
+    fun provideWorkManager(@ApplicationContext context: Context): WorkManager {
+        return WorkManager.getInstance(context)
+    }
+}
