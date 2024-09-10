b5b6ecae857e98e051e7f173102202e95547c27c
GongDoMin <yhw198@naver.com>
2024-09-10 20:17:37 +0900

    feat: 기존의 PagingSource 와 달리 database 에 대한 접근이 필요하므로 Repository 를 RemoteMediator 로 변경

--- data/src/main/java/com/prac/data/repository/RepoRepository.kt
+++ data/src/main/java/com/prac/data/repository/RepoRepository.kt
@@ -1,18 +1,22 @@
 package com.prac.data.repository
 
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.PagingData
+import androidx.paging.RemoteMediator
 import com.prac.data.entity.RepoDetailEntity
 import com.prac.data.entity.RepoEntity
+import com.prac.data.source.local.room.entity.Repository
 import kotlinx.coroutines.flow.Flow
 
-interface RepoRepository {
-    suspend fun getRepositories() : Flow<PagingData<RepoEntity>>
+@OptIn(ExperimentalPagingApi::class)
+abstract class RepoRepository : RemoteMediator<Int, Repository>() {
+    abstract suspend fun getRepositories() : Flow<PagingData<RepoEntity>>
 
-    suspend fun getRepository(userName: String, repoName: String) : Result<RepoDetailEntity>
+    abstract suspend fun getRepository(userName: String, repoName: String) : Result<RepoDetailEntity>
 
-    suspend fun isStarred(repoName: String) : Result<Boolean>
+    abstract suspend fun isStarred(repoName: String) : Result<Boolean>
 
-    suspend fun starRepository(userName: String, repoName: String) : Result<Unit>
+    abstract suspend fun starRepository(userName: String, repoName: String) : Result<Unit>
 
-    suspend fun unStarRepository(userName: String, repoName: String) : Result<Unit>
+    abstract suspend fun unStarRepository(userName: String, repoName: String) : Result<Unit>
 }
